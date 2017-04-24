dd6f18dd12a6dacb27b0c4cb92db7f6c6bf352b4
yao <chuanyuyao123@163.com>
2017-04-24 15:42:55 +0800

    create ML-alg

---
+++ test.py
@@ -0,0 +1,19 @@
+"""
+load from a txt file,
+data structure is matrix,
+plot graph with networkX.
+
+author: Chuanyu Yao
+email:	chuanyuyao123@163.com
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def plot_graph(str)
+    graph_np=np.loadtxt(str);
+	graph=nx.from_numpy_matrix(graph_np)
+	nx.draw(graph)
+	plt.show()
+	
